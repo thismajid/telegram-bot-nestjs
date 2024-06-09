42ef5104c2d3d6bfcb4406dcdc4b8ecb694500b1
Majid Khorshidian <mkhorshidian72@gmail.com>
2024-06-09 11:29:19 +0330

    Chore: Adding job manager module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { BotModule } from './modules/bot/bot.module';
+import { JobManagerModule } from './modules/job-manager/job-manager.module';
 
 @Module({
-  imports: [BotModule],
+  imports: [BotModule, JobManagerModule],
 })
 export class AppModule {}
